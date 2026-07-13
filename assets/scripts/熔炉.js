039eb0eb6fdda015bbbee0870cc5deb212c9299d
luoys121005 <3647853715@qq.com>
2026-07-13 22:27:15 +0800

    修复熔炉

--- assets/scripts/熔炉.js
+++ assets/scripts/熔炉.js
@@ -8,7 +8,8 @@ const furnace = library.MultiCrafter(
     [
         {
             input: {
-                items: ["泰拉世纪工厂-铜矿/5"],
+                items: ["铜矿/5"],
+                liquids: [],
             },
             output: {
                 items: ["copper/1"],
@@ -18,27 +19,27 @@ const furnace = library.MultiCrafter(
        
         {
             input: {
-                items: ["泰拉世纪工厂-锡矿/3"],
+                items: ["锡矿/3"],
             },
             output: {
-                items: ["泰拉世纪工厂-锡锭/1"],
+                items: ["锡锭/1"],
             },
             craftTime: 5,
         },
         
         {
             input: {
-                items: ["泰拉世纪工厂-铁矿/3"],
+                items: ["铁矿/3"],
             },
             output: {
-                items: ["泰拉世纪工厂-铁锭/1"],
+                items: ["铁锭/1"],
             },
             craftTime: 5,
         },
         
         {
             input: {
-                items: ["泰拉世纪工厂-铅矿/3"],
+                items: ["铅矿/3"],
             },
             output: {
                 items: ["lead/1"],
@@ -48,59 +49,59 @@ const furnace = library.MultiCrafter(
         
         {
             input: {
-                items: ["泰拉世纪工厂-银矿/4"],
+                items: ["银矿/4"],
             },
             output: {
-                items: ["泰拉世纪工厂-银锭/1"],
+                items: ["银锭/1"],
             },
             craftTime: 5,
         },
         
         {
             input: {
-                items: ["泰拉世纪工厂-钨矿/4"],
+                items: ["钨矿/4"],
             },
             output: {
-                items: ["泰拉世纪工厂-钨锭/1"],
+                items: ["钨锭/1"],
             },
             craftTime: 5,
         },
         
         {
             input: {
-                items: ["泰拉世纪工厂-金矿/4"],
+                items: ["金矿/4"],
             },
             output: {
-                items: ["泰拉世纪工厂-金锭/1"],
+                items: ["金锭/1"],
             },
             craftTime: 5,
         },
         
         {
             input: {
-                items: ["泰拉世纪工厂-铂金矿/4"],
+                items: ["铂金矿/4"],
             },
             output: {
-                items: ["泰拉世纪工厂-铂金锭/1"],
+                items: ["铂金锭/1"],
             },
             craftTime: 5,
         },
         {
             input: {
-                items: ["泰拉世纪工厂-魔矿/3"],
+                items: ["魔矿/3"],
             },
             output: {
-                items: ["泰拉世纪工厂-魔锭/1"],
+                items: ["魔锭/1"],
             },
             craftTime: 5,
         },
         // 以下为新增的腥红矿与魔矿合成配方
         {
             input: {
-                items: ["泰拉世纪工厂-腥红矿/3"],
+                items: ["腥红矿/3"],
             },
             output: {
-                items: ["泰拉世纪工厂-腥红锭/1"],
+                items: ["腥红锭/1"],
             },
             craftTime: 5,
         }
