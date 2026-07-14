e13c8e4323a0d5e7e1a0fda0f69939de2eb233e3
lys121005 <3647853715@qq.com>
2026-07-14 16:05:10 +0800

    修复bug

--- assets/scripts/熔炉.js
+++ assets/scripts/熔炉.js
@@ -8,7 +8,7 @@ const furnace = library.MultiCrafter(
     [
         {
             input: {
-                items: ["铜矿/5"],
+                items: ["TerrariaforMindustry-铜矿/5"],
                 liquids: [],
             },
             output: {
@@ -19,27 +19,27 @@ const furnace = library.MultiCrafter(
        
         {
             input: {
-                items: ["锡矿/3"],
+                items: ["TerrariaforMindustry-锡矿/3"],
             },
             output: {
-                items: ["锡锭/1"],
+                items: ["TerrariaforMindustry-锡锭/1"],
             },
             craftTime: 5,
         },
         
         {
             input: {
-                items: ["铁矿/3"],
+                items: ["TerrariaforMindustry-铁矿/3"],
             },
             output: {
-                items: ["铁锭/1"],
+                items: ["TerrariaforMindustry-铁锭/1"],
             },
             craftTime: 5,
         },
         
         {
             input: {
-                items: ["铅矿/3"],
+                items: ["TerrariaforMindustry-铅矿/3"],
             },
             output: {
                 items: ["lead/1"],
@@ -49,59 +49,59 @@ const furnace = library.MultiCrafter(
         
         {
             input: {
-                items: ["银矿/4"],
+                items: ["TerrariaforMindustry-银矿/4"],
             },
             output: {
-                items: ["银锭/1"],
+                items: ["TerrariaforMindustry-银锭/1"],
             },
             craftTime: 5,
         },
         
         {
             input: {
-                items: ["钨矿/4"],
+                items: ["TerrariaforMindustry-钨矿/4"],
             },
             output: {
-                items: ["钨锭/1"],
+                items: ["TerrariaforMindustry-钨锭/1"],
             },
             craftTime: 5,
         },
         
         {
             input: {
-                items: ["金矿/4"],
+                items: ["TerrariaforMindustry-金矿/4"],
             },
             output: {
-                items: ["金锭/1"],
+                items: ["TerrariaforMindustry-金锭/1"],
             },
             craftTime: 5,
         },
         
         {
             input: {
-                items: ["铂金矿/4"],
+                items: ["TerrariaforMindustry-铂金矿/4"],
             },
             output: {
-                items: ["铂金锭/1"],
+                items: ["TerrariaforMindustry-铂金锭/1"],
             },
             craftTime: 5,
         },
         {
             input: {
-                items: ["魔矿/3"],
+                items: ["TerrariaforMindustry-魔矿/3"],
             },
             output: {
-                items: ["魔锭/1"],
+                items: ["TerrariaforMindustry-魔锭/1"],
             },
             craftTime: 5,
         },
         // 以下为新增的腥红矿与魔矿合成配方
         {
             input: {
-                items: ["腥红矿/3"],
+                items: ["TerrariaforMindustry-腥红矿/3"],
             },
             output: {
-                items: ["腥红锭/1"],
+                items: ["TerrariaforMindustry-腥红锭/1"],
             },
             craftTime: 5,
         }
